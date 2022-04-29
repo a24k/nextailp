fd39dfebf82a1565348c061f896ace90716c14f6
Atsushi KAWASAKI <ak@xlix.org>
2022-04-29 22:43:30 +0900

    fineup css.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 const Index: NextPage = () => {
-  const router = useRouter();
-
   return (
     <>
       <Head>
@@ -20,7 +17,6 @@ const Index: NextPage = () => {
 
         <div className="card">
           <h1><Link href="https://nextailp.pages.dev"><a>nextailp.pages.dev</a></Link></h1>
-          <h2>{router.pathname}</h2>
         </div>
 
         <p>powered by <Link href="https://github.com/a24k/nextailp"><a>nextailp</a></Link> - a domain parking example based on <Link href="https://nextjs.org/"><a>Next.js</a></Link> & <Link href="https://tailwindcss.com/"><a>Tailwind CSS</a></Link>.</p>
