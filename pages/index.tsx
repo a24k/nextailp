839010cf83a84f182cd852c960071af6b61806cf
Atsushi KAWASAKI <ak@xlix.org>
2022-05-01 20:23:34 +0900

    show url using client-side fetching.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 const Index: NextPage = () => {
+  const [url, setUrl] = useState<string>('/');
+
+  useEffect(() => {
+    setUrl(document.URL);
+  });
+
   return (
     <>
       <Head>
@@ -17,6 +24,7 @@ const Index: NextPage = () => {
 
         <div className="card">
           <h1><Link href="https://nextailp.pages.dev"><a>nextailp.pages.dev</a></Link></h1>
+          <h2>{url}</h2>
         </div>
 
         <p>powered by <Link href="https://github.com/a24k/nextailp"><a>nextailp</a></Link> - a domain parking example based on <Link href="https://nextjs.org/"><a>Next.js</a></Link> & <Link href="https://tailwindcss.com/"><a>Tailwind CSS</a></Link>.</p>
